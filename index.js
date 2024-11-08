5fb5ddfa383788a671e769e18c80bfc0a5316781
a52cents <96891277+a52cents@users.noreply.github.com>
2024-11-08 10:09:57 +0100

    new file:   .env 	modified:   .gitignore 	modified:   index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ registerCategoryRoutes(fastify);
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost'
+  });
   await fastify.ready()
 } catch (err) {
   fastify.log.error(err);
